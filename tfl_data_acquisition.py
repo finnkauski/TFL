9dcdae2a141e76b8adc7dae2f3fa9fbebeb76638
Arthur <iwiiv@gmail.com>
2018-05-07 18:31:37 +0100

    Added MT Proper

--- tfl_data_acquisition.py
+++ tfl_data_acquisition.py
@@ -123,6 +123,10 @@ class CamSession():
         
         print("____images_downloaded____")
         
+        
+    def download_media_mt(self, url_col, path):
+        
+        pass
 
 
 
@@ -130,7 +134,7 @@ if __name__ == "__main__":
     
     obj = CamSession()
     obj.parse()
-    obj.download_media("imageUrl", r"C:\Users\Admin\Documents\TfLdata\TFL\images")
+#    obj.download_media("imageUrl", r"E:\Code\Python\TFL\images")
 #    obj.download_media("videoUrl", "/home/finn/Desktop/Link to TFL/videos")
     result = obj.raw_json
     df = obj.parsed_df
@@ -138,5 +142,56 @@ if __name__ == "__main__":
     
 
     
+#%%
+    
 
+import time
+    
+def fetch(url):
+    
+    file_name = urllib.parse.urlsplit(url).path.split(sep = "/")[-1]
+            
+    urllib.request.urlretrieve(url, "E:/Code/Python/TFL/images/" + file_name)
+    
+    time.sleep(1)
+import concurrent.futures as f
 
+def main():
+    
+    with f.ThreadPoolExecutor(16) as pool:
+        
+        pool.map(fetch, df["imageUrl"])
+
+%time main()
+#    pool.shutdown(wait = True)
+
+#%%
+    
+import concurrent.futures
+import math
+ 
+PRIMES = [
+    112272535095293,
+    112582705942171,
+    112272535095293,
+    115280095190773,
+    115797848077099,
+    1099726899285419]
+ 
+def is_prime(n):
+    if n % 2 == 0:
+        return False
+ 
+    sqrt_n = int(math.floor(math.sqrt(n)))
+    for i in range(3, sqrt_n + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+ 
+def main():
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):
+            print('%d is prime: %s' % (number, prime))
+ 
+if __name__ == '__main__':
+    main()
